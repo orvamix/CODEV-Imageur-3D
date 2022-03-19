6c8eebb3799228f9af4d85891906917ee74afc9d
orvamix <9323776+orvamix@users.noreply.github.com>
2022-03-19 20:11:43 +0100

    semi-victoire

--- coord3D_objet.py
+++ coord3D_objet.py
@@ -7,6 +7,6 @@ def coord3D(u,v, v_e):
     
     coord_px=np.array([u,v,v_e])
     G=np.array([[MR[2][0], MR[2][0], ME[2][0]], [MR[2][1], MR[2][1], ME[2][1]], [MR[2][2], MR[2][2], ME[2][2]]])*coord_px-np.array([[MR[0][0], MR[1][0], ME[1][0]], [MR[0][1], MR[1][1], ME[1][1]], [MR[0][2], MR[1][2], ME[1][2]]])
-    H=np.array([MR[0][4],MR[1][4],ME[1][4]])-coord_px
+    H=np.array([MR[0][3],MR[1][3],ME[1][3]])-coord_px
     G_inv=np.linalg.inv(G)
-    return G_inv*H
+    return np.matmul(G_inv, H)
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 # On importe le module matplotlib qui permet de générer des graphiques 2D et 3D
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+
 import matplotlib.pyplot as plt
 from fringe_detector import *
 from coord3D_objet import *
@@ -13,4 +18,40 @@ uRzoom=info['uRzoom']
 vRzoom=info['vRzoom']
 
 
-fringe_detector("IRZoom",N,uRzoom,vRzoom)
+Mat_temp=[]
+Mat_fin=[]
+
+# fringe_detector("IRZoom",N,uRzoom,vRzoom)
+
+PosD=np.loadtxt('results/PosiDroiteIRZoom.txt')
+
+for i in range(1, 2**N):
+    v_e=(i+1)*vRzoom/(2**N)
+    for j in range(len(PosD)):
+        for k in range(len(PosD[j])):
+            if PosD[j][k]== i:
+                Mat_temp.append([j+1,k+1,v_e])
+
+for i in range(len(Mat_temp)):
+    Mat_fin.append(coord3D(Mat_temp[i][0],Mat_temp[i][1],Mat_temp[i][2]).tolist())
+    
+with open("mat.txt", 'w') as output:
+    for row in Mat_fin:
+        output.write(str(row) + '\n')
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x =[item[0] for item in Mat_fin]
+y =[item[1] for item in Mat_fin]
+z =[item[2] for item in Mat_fin]
+
+
+
+ax.scatter(x, y, z, c='r', marker='o',s=0.1)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()

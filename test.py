553b1db3e05ccc81c56dc2b90dce5a85b510e55a
mguzmanz <102219737+mguzmanz@users.noreply.github.com>
2022-03-23 11:28:30 +0100

    Update test.py

--- test.py
+++ test.py
@@ -22,8 +22,9 @@ def coord3D(u,v, v_e):
     G=np.array([[MR[2][0]*coord_px[0], MR[2][1]*coord_px[0], MR[2][2]*coord_px[0]], [MR[2][0]*coord_px[1], MR[2][1]*coord_px[1], MR[2][2]*coord_px[1]], [ME[2][0]*coord_px[2], ME[2][1]*coord_px[2], ME[2][2]*coord_px[2]]])
     G=np.subtract(G,np.array([[MR[0][0], MR[0][1], MR[0][2]], [MR[1][0], MR[1][1], MR[1][2]], [ME[1][0], ME[1][1], ME[1][2]]]))
     coord_pxx=np.array([[MR[2][3]*coord_px[0]], [MR[2][3]*coord_px[1]], [ME[2][3]*coord_px[2]]])
-    H=np.subtract(np.array([MR[0][3],MR[1][3],ME[1][3]]),coord_pxx)
+    H=np.subtract(np.array([[MR[0][3]],[MR[1][3]],[ME[1][3]]]),coord_pxx)
     G_inv=np.linalg.inv(G)
+    print(G)
     return np.matmul(G_inv, H)
 
 

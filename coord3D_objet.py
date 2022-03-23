06c3674a1bf93e855b40d8ceea1470be78baec83
orvamix <9323776+orvamix@users.noreply.github.com>
2022-03-23 10:24:05 +0100

    pour alice

--- coord3D_objet.py
+++ coord3D_objet.py
@@ -6,6 +6,7 @@ def coord3D(u,v, v_e):
     MR=np.loadtxt('MR.txt')
     
     coord_px=np.array([u,v,v_e])
+    print(coord_px)
     G=np.array([[MR[2][0]*coord_px[0], MR[2][0]*coord_px[1], ME[2][0]*coord_px[2]], [MR[2][1]*coord_px[0], MR[2][1]*coord_px[1], ME[2][1]*coord_px[2]], [MR[2][2]*coord_px[0], MR[2][2]*coord_px[1], ME[2][2]*coord_px[2]]])
     G=np.subtract(G,np.array([[MR[0][0], MR[1][0], ME[1][0]], [MR[0][1], MR[1][1], ME[1][1]], [MR[0][2], MR[1][2], ME[1][2]]]))
     H=np.subtract(np.array([MR[0][3],MR[1][3],ME[1][3]]),coord_px)
